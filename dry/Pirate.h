ea015768d11b95bc19851fa42e31e3b1012306c6
Space-Gourmet <eyalrevah27@gmail.com>
2025-05-28 20:11:32 +0300

    The One Piece is real!

--- dry/Pirate.h
+++ dry/Pirate.h
@@ -8,15 +8,22 @@ using std::string;
 class Pirate {
 private:
     string name;
+    int bounty;
 
 public:
-    Pirate(const string& name);
+    Pirate(const string& name, const int bounty);
+
+    Pirate() = default;
 
     ~Pirate() = default;
 
     void setName(const string& name);
 
+    void setBounty(const int bounty);
+
     string getName();
+
+    int getBounty();
 
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
 };
